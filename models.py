8e37b4d941f2b304b5954b52572693f45d70b02b
Psosv3 <contact@onexus.info>
2025-06-15 22:06:07 +0200

    bug resolved: code 500 error on get_answer

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 from langchain_mistralai.chat_models import ChatMistralAI
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+assert os.getenv("OPENAI_API_KEY") 
+assert os.getenv("MISTRAL_API_KEY") 
 
 openai_key = os.getenv("OPENAI_API_KEY")
 mistral_api_key = os.getenv("MISTRAL_API_KEY")
